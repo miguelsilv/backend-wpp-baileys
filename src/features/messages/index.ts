a0930925b3cd03897560ce7a1dbb624a777cba70
Miguel Silva <miguelsilv98@gmail.com>
2025-02-18 16:30:02 -0300

    fix: adicionar service de mensagem no modulo

--- src/features/messages/index.ts
+++ src/features/messages/index.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { MessagesController } from "./messages.controller";
 import { PrismaService } from "../../prisma.service";
-
+import { MessagesService } from "./messages.services";
 @Module({
     controllers: [MessagesController],
-    providers: [PrismaService],
+    providers: [PrismaService, MessagesService],
 })
 export class MessagesModule { }
